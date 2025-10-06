e7a87ed2a532d330eba8565789e2d4af8725d09c
Haris <160738454+unusedusername01@users.noreply.github.com>
2025-10-06 11:00:49 +0200

    feat: add RequestRepository and UserProfileRepository interfaces (#18)

---
+++ app/src/main/java/com/android/sample/model/request/RequestRepository.kt
@@ -0,0 +1,47 @@
+package com.android.sample.model.request
+
+/** Repository interface for managing requests. */
+interface RequestRepository {
+  /** Generates and returns a new unique identifier for a request. */
+  fun getNewRequestId(): String
+
+  /**
+   * Retrieves all requests from the repository.
+   *
+   * @return A list of all requests.
+   */
+  suspend fun getAllRequests(): List<Request>
+
+  /**
+   * Retrieves a specific request by its unique identifier.
+   *
+   * @param requestId The unique identifier of the request to retrieve.
+   * @return The Request with the specified ID.
+   * @throws Exception if the request is not found.
+   */
+  suspend fun getRequest(requestId: String): Request
+
+  /**
+   * Adds a new request to the repository.
+   *
+   * @param request The Request to add.
+   */
+  suspend fun addRequest(request: Request)
+
+  /**
+   * Updates an existing request in the repository.
+   *
+   * @param requestId The unique identifier of the request to update.
+   * @param updatedRequest The new state of the request.
+   * @throws Exception if the request is not found.
+   */
+  suspend fun updateRequest(requestId: String, updatedRequest: Request)
+
+  /**
+   * Deletes a request from the repository.
+   *
+   * @param requestId The unique identifier of the request to delete.
+   * @throws Exception if the request is not found.
+   */
+  suspend fun deleteRequest(requestId: String)
+}
