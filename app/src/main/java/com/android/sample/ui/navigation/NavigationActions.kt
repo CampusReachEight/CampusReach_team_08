217b7b68818dc858113bfdaaac61f97206d454d9
Maxime-T <maxime.tscharner@gmail.com>
2025-10-07 12:33:35 +0200

    feat(navigation): add sealed class defining navigation route and behaviour

---
+++ app/src/main/java/com/android/sample/ui/navigation/NavigationActions.kt
@@ -0,0 +1,35 @@
+package com.android.sample.ui.navigation
+
+import androidx.navigation.NavHostController
+
+sealed class Screen(val route: String, val doesResetStack: Boolean = false) {
+  object Login : Screen(route = "login", doesResetStack = true)
+
+  object Requests : Screen(route = "requests", doesResetStack = true)
+
+  object Events : Screen(route = "events", doesResetStack = true)
+
+  object Map : Screen(route = "map", doesResetStack = true)
+
+  object Profile : Screen(route = "profile")
+
+  object AddRequest : Screen(route = "add_request")
+
+  object AddEvent : Screen(route = "add_event")
+
+  data class EditRequest(val requestId: String) : Screen(route = "edit_request/${requestId}") {
+    companion object {
+      const val ARG_REQUEST_ID = "requestId"
+      const val route = "edit_request/{$ARG_REQUEST_ID}"
+    }
+  }
+
+  data class EditEvent(val eventId: String) : Screen(route = "edit_event/${eventId}") {
+    companion object {
+      const val ARG_EVENT_ID = "eventId"
+      const val route = "edit_event/{$ARG_EVENT_ID}"
+    }
+  }
+}
+
+
