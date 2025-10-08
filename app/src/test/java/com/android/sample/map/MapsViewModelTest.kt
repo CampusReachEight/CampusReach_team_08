8c59b830d630d04e6faa450027b91e07a49edc7b
Nolan <nolan.furst@epfl.ch>
2025-10-08 22:23:46 +0200

    Edit MapsViewModelTest.kt
    Made it into a unit Test, and move it into test[UnitTest]

--- app/src/test/java/com/android/sample/map/MapsViewModelTest.kt
+++ app/src/test/java/com/android/sample/map/MapsViewModelTest.kt
@@ -1,16 +1,13 @@
 package com.android.sample.map
 
-import androidx.compose.ui.test.junit4.createComposeRule
 import com.android.sample.model.map.Location
 import com.android.sample.model.request.Request
 import com.android.sample.model.request.RequestRepository
 import com.android.sample.model.request.RequestStatus
-import com.android.sample.ui.map.MapScreen
 import com.android.sample.ui.map.MapViewModel
 import com.google.android.gms.maps.model.LatLng
 import java.util.UUID
-import junit.framework.TestCase.assertNotNull
-import junit.framework.TestCase.assertNull
+import junit.framework.TestCase
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.test.StandardTestDispatcher
@@ -19,14 +16,11 @@ import kotlinx.coroutines.test.resetMain
 import kotlinx.coroutines.test.runTest
 import kotlinx.coroutines.test.setMain
 import org.junit.After
-import org.junit.Assert.assertEquals
+import org.junit.Assert
 import org.junit.Before
-import org.junit.Rule
 import org.junit.Test
 
 class MapsViewModelTest {
-
-  @get:Rule val composeTestRule = createComposeRule()
 
   // Fake repository used for testing/development because the real one is not implemented yet
   private class FakeRequestRepository : RequestRepository {
@@ -153,19 +147,18 @@ class MapsViewModelTest {
 
     viewModel = MapViewModel(fakeRepository)
 
-    assertNull(viewModel.uiState.value.errorMsg)
+    TestCase.assertNull(viewModel.uiState.value.errorMsg)
 
-    composeTestRule.setContent { MapScreen(viewModel) }
-    assertNull(viewModel.uiState.value.errorMsg)
+    TestCase.assertNull(viewModel.uiState.value.errorMsg)
     fakeRepository.setShouldThrowError(true)
 
     runTest {
       viewModel.refreshUIState()
       advanceUntilIdle()
-      assertNotNull(viewModel.uiState.value.errorMsg)
+      TestCase.assertNotNull(viewModel.uiState.value.errorMsg)
       viewModel.clearErrorMsg()
       advanceUntilIdle()
-      assertNull(viewModel.uiState.value.errorMsg)
+      TestCase.assertNull(viewModel.uiState.value.errorMsg)
     }
   }
 
@@ -175,13 +168,12 @@ class MapsViewModelTest {
   fun baseRequestsIsGood() {
     fakeRepository.setShouldThrowError(false)
     viewModel = MapViewModel(fakeRepository)
-    composeTestRule.setContent { MapScreen(viewModel) }
 
     runTest {
       viewModel.refreshUIState()
       advanceUntilIdle()
 
-      assertEquals(
+      Assert.assertEquals(
           LatLng(EPFL_LOCATION.latitude, EPFL_LOCATION.longitude), viewModel.uiState.value.target)
     }
   }
@@ -191,13 +183,12 @@ class MapsViewModelTest {
   fun addRequestsIsGood() {
     fakeRepository.setShouldThrowError(false)
     viewModel = MapViewModel(fakeRepository)
-    composeTestRule.setContent { MapScreen(viewModel) }
     runTest {
       fakeRepository.addRequest(request1)
       viewModel.refreshUIState()
       advanceUntilIdle()
-      assertEquals(LatLng(10.0, 10.0), viewModel.uiState.value.target)
-      assertEquals(listOf(request1), viewModel.uiState.value.request)
+      Assert.assertEquals(LatLng(10.0, 10.0), viewModel.uiState.value.target)
+      Assert.assertEquals(listOf(request1), viewModel.uiState.value.request)
       fakeRepository.deleteRequest(request1.requestId)
     }
   }
@@ -207,14 +198,13 @@ class MapsViewModelTest {
   fun editRequestsIsGood() {
     fakeRepository.setShouldThrowError(false)
     viewModel = MapViewModel(fakeRepository)
-    composeTestRule.setContent { MapScreen(viewModel) }
     runTest {
       fakeRepository.addRequest(request1)
       fakeRepository.updateRequest("1", request3)
       viewModel.refreshUIState()
       advanceUntilIdle()
-      assertEquals(LatLng(0.0, 50.0), viewModel.uiState.value.target)
-      assertEquals(listOf(request3), viewModel.uiState.value.request)
+      Assert.assertEquals(LatLng(0.0, 50.0), viewModel.uiState.value.target)
+      Assert.assertEquals(listOf(request3), viewModel.uiState.value.request)
       fakeRepository.deleteRequest(request3.requestId)
     }
   }
@@ -224,15 +214,14 @@ class MapsViewModelTest {
   fun deleteRequestsIsGood() {
     fakeRepository.setShouldThrowError(false)
     viewModel = MapViewModel(fakeRepository)
-    composeTestRule.setContent { MapScreen(viewModel) }
     runTest {
       fakeRepository.addRequest(request3)
       fakeRepository.addRequest(request2)
       fakeRepository.deleteRequest("3")
       viewModel.refreshUIState()
       advanceUntilIdle()
-      assertEquals(LatLng(20.0, 20.0), viewModel.uiState.value.target)
-      assertEquals(listOf(request2), viewModel.uiState.value.request)
+      Assert.assertEquals(LatLng(20.0, 20.0), viewModel.uiState.value.target)
+      Assert.assertEquals(listOf(request2), viewModel.uiState.value.request)
       fakeRepository.deleteRequest(request2.requestId)
     }
   }
