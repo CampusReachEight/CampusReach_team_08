ee7f47f7095978115e97cdc426d7de5e39395c08
Maxime Tscharner <85079922+Maxime-T@users.noreply.github.com>
2025-10-08 12:21:58 +0200

    Feature/navigation actions (#29)
    * feat(navigation): add sealed class defining navigation route and behaviour
    
    * feat(navigation): implement NavigationActions class for screen navigation and back stack management
    
    * feat(navigation): add NavigationScreen composable
    
    implemented with placeholders for testing purpose
    
    * feat(navigation): update screen routes and improve navigation logic
    
    * feat(navigation): enhance navigation structure with NavigationType and update screen routes
    
    * fix(navigation): remove unused parameters and add profile navigation test cases
    
    * refactor(navigation): clean up imports in NavigationScreen.kt
    
    * fix(navigation): increase code coverage
    
    * fix(navigation): format code
    
    * fix(navigation): uncomment navigationActions
    
    * fix(navigation): remove unused navigationActions parameter from NavigationScreen
    
    * fix(navigation): adjust comment formatting
    
    * fix(navigation): update parameter documentation and restore navigationActions usage
    
    * fix(navigation): reformated documentation for Screen

---
+++ app/src/main/java/com/android/sample/ui/navigation/NavigationActions.kt
@@ -0,0 +1,119 @@
+package com.android.sample.ui.navigation
+
+import androidx.navigation.NavHostController
+
+/**
+ * Class defining the different screens in the app
+ *
+ * @param route Navigation route for the screen
+ * @param navigationType Type of navigation for the screen, see [NavigationType]
+ */
+sealed class Screen(
+    val route: String,
+    val navigationType: NavigationType = NavigationType.SUB_SCREEN
+) {
+  object Login : Screen(route = "login/main", NavigationType.APP_ENTRY_POINT)
+
+  object Requests : Screen(route = "requests/main", NavigationType.APP_ENTRY_POINT)
+
+  object Events : Screen(route = "events/main", NavigationType.TAB)
+
+  object Map : Screen(route = "map/main", NavigationType.TAB)
+
+  object AddRequest : Screen(route = "requests/add")
+
+  object AddEvent : Screen(route = "events/add")
+
+  data class RequestDetails(val requestId: String) :
+      Screen(route = "requests/details/${requestId}") {
+    companion object {
+      const val ARG_REQUEST_ID = "requestId"
+      const val route = "requests/details/{$ARG_REQUEST_ID}"
+    }
+  }
+
+  data class EventDetails(val eventId: String) : Screen(route = "events/details/${eventId}") {
+    companion object {
+      const val ARG_EVENT_ID = "eventId"
+      const val route = "events/details/{$ARG_EVENT_ID}"
+    }
+  }
+
+  data class Profile(val userId: String) : Screen(route = "profile/main/${userId}") {
+    companion object {
+      const val ARG_USER_ID = "userId"
+      const val route = "profile/main/{$ARG_USER_ID}"
+    }
+  }
+}
+
+/**
+ * Defines how the screen is treated in the navigation stack
+ * - TAB: Bottom navigation tab, navigating to a tab clears the back stack, sets Requests as the
+ *   root, then adds the tab on top
+ * - APP_ENTRY_POINT: App entry point, navigating to an entry point clears the back stack and sets
+ *   the screen as the root
+ * - SUB_SCREEN: Regular screen, navigating to a sub screen adds it to the back stack (navigating to
+ *   a sub screen from a tab not associated with it will still put it on top of the stack without
+ *   clearing it)
+ */
+enum class NavigationType {
+  TAB,
+  APP_ENTRY_POINT,
+  SUB_SCREEN,
+}
+
+/**
+ * Class holding navigation actions this the main way to navigate between screens in the app
+ *
+ * @param navController NavHostController to be used for navigation actions
+ */
+open class NavigationActions(private val navController: NavHostController) {
+
+  open fun navigateTo(screen: Screen) {
+    if (currentRoute() == screen.route) {
+      return
+    }
+
+    when (screen.navigationType) {
+      NavigationType.TAB -> {
+        // Clear stack and set Requests as the root
+        navController.navigate(Screen.Requests.route) {
+          popUpTo(0) { inclusive = true }
+          restoreState = false
+          launchSingleTop = true
+        }
+
+        // Navigate to the tab
+        navController.navigate(screen.route) {
+          launchSingleTop = true
+          restoreState = false
+        }
+      }
+      NavigationType.APP_ENTRY_POINT -> {
+        navController.navigate(screen.route) {
+          popUpTo(0) { inclusive = true }
+          restoreState = false
+          launchSingleTop = true
+        }
+      }
+      NavigationType.SUB_SCREEN -> {
+        navController.navigate(screen.route) {
+          launchSingleTop = true
+          restoreState = false
+        }
+      }
+    }
+  }
+
+  /**
+   * Navigate back to previous screen
+   *
+   * contrary to the system back button, this does not close the app
+   */
+  open fun goBack() {
+    navController.popBackStack()
+  }
+
+  open fun currentRoute(): String = navController.currentBackStackEntry?.destination?.route ?: ""
+}
---
+++ app/src/main/java/com/android/sample/ui/navigation/NavigationScreen.kt
@@ -0,0 +1,91 @@
+package com.android.sample.ui.navigation
+
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.testTag
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navigation
+import com.google.firebase.auth.FirebaseAuth
+
+@Composable
+fun NavigationScreen(
+    modifier: Modifier = Modifier,
+    navController: NavHostController = rememberNavController(),
+    navigationActions: NavigationActions = NavigationActions(navController)
+) {
+
+  val startDestination = if (FirebaseAuth.getInstance().currentUser == null) "login" else "requests"
+
+  NavHost(
+      navController = navController,
+      startDestination = startDestination,
+      modifier = modifier,
+  ) {
+    navigation(startDestination = Screen.Login.route, route = "login") {
+      composable(Screen.Login.route) {
+        PlaceHolderScreen(text = "Login Screen", Modifier.testTag(NavigationTestTags.LOGIN_SCREEN))
+      }
+    }
+
+    navigation(startDestination = Screen.Requests.route, route = "requests") {
+      composable(Screen.Requests.route) {
+        PlaceHolderScreen(
+            text = "Requests Screen", Modifier.testTag(NavigationTestTags.REQUESTS_SCREEN))
+      }
+      composable(Screen.AddRequest.route) {
+        PlaceHolderScreen(
+            text = "Add Request Screen", Modifier.testTag(NavigationTestTags.ADD_REQUEST_SCREEN))
+      }
+      composable(Screen.RequestDetails.route) { navBackStackEntry ->
+        val requestId = navBackStackEntry.arguments?.getString(Screen.RequestDetails.ARG_REQUEST_ID)
+        PlaceHolderScreen(
+            text = "Edit Request Screen: $requestId",
+            Modifier.testTag(NavigationTestTags.EDIT_REQUEST_SCREEN))
+      }
+    }
+
+    navigation(startDestination = Screen.Events.route, route = "events") {
+      composable(Screen.Events.route) {
+        PlaceHolderScreen(
+            text = "Events Screen", Modifier.testTag(NavigationTestTags.EVENTS_SCREEN))
+      }
+      composable(Screen.AddEvent.route) {
+        PlaceHolderScreen(
+            text = "Add Event Screen", Modifier.testTag(NavigationTestTags.ADD_EVENT_SCREEN))
+      }
+      composable(Screen.EventDetails.route) { navBackStackEntry ->
+        val eventId = navBackStackEntry.arguments?.getString(Screen.EventDetails.ARG_EVENT_ID)
+        PlaceHolderScreen(
+            text = "Edit Event Screen : $eventId",
+            Modifier.testTag(NavigationTestTags.EDIT_EVENT_SCREEN))
+      }
+    }
+
+    navigation(startDestination = Screen.Profile.route, route = "profile") {
+      composable(Screen.Profile.route) { navBackStackEntry ->
+        val userId = navBackStackEntry.arguments?.getString(Screen.Profile.ARG_USER_ID)
+        PlaceHolderScreen(
+            text = "Profile Screen: $userId", Modifier.testTag(NavigationTestTags.PROFILE_SCREEN))
+      }
+    }
+
+    navigation(startDestination = Screen.Map.route, route = "map") {
+      composable(Screen.Map.route) {
+        PlaceHolderScreen(text = "Map Screen", Modifier.testTag(NavigationTestTags.MAP_SCREEN))
+      }
+    }
+  }
+}
+
+@Composable
+fun PlaceHolderScreen(text: String, modifier: Modifier) {
+  Text(text = text, textAlign = TextAlign.Center, modifier = modifier.fillMaxSize().padding(20.dp))
+}
