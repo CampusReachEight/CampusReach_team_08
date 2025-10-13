06e492b4a8409ee8ca9192cb26e40986c585f78c
Cyriac <cyriacph@gmail.com>
2025-10-13 16:35:31 +0200

    chore: run build after merge to verify project integrity

--- app/src/main/java/com/android/sample/ui/profile/Profile.kt
+++ app/src/main/java/com/android/sample/ui/profile/Profile.kt
@@ -26,7 +26,6 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
-import com.android.sample.ui.navigation.NavigationTestTags
 
 object ProfileTestTags {
   const val PROFILE_HEADER = "profile_header"
