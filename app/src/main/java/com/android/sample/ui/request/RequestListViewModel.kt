58558aadf73979db22db90eba38c0b089eb6ec02
Maxime-T <maxime.tscharner@gmail.com>
2025-10-29 14:51:10 +0100

    fix(profile): formated code

--- app/src/main/java/com/android/sample/ui/request/RequestListViewModel.kt
+++ app/src/main/java/com/android/sample/ui/request/RequestListViewModel.kt
@@ -18,8 +18,8 @@ import com.google.firebase.auth.ktx.auth
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 import java.net.URL
-import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.SharingStarted
