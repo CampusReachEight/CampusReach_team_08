f091bb765a2476b1fa3d42d996bbdf7334f4c0c9
Cyriac <cyriacph@gmail.com>
2025-11-07 16:39:08 +0100

    chore: run format and build

--- app/src/main/java/com/android/sample/model/profile/setup/SetupProfileRepository.kt
+++ app/src/main/java/com/android/sample/model/profile/setup/SetupProfileRepository.kt
@@ -3,6 +3,7 @@ package com.android.sample.model.profile.setup
 import com.android.sample.ui.profile.setup.SetupProfileState
 
 interface SetupProfileRepository {
-    suspend fun getProfile(): SetupProfileState
-    suspend fun saveProfile(state: SetupProfileState)
+  suspend fun getProfile(): SetupProfileState
+
+  suspend fun saveProfile(state: SetupProfileState)
 }
