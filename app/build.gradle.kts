e061bdb653af191a1566e487987f009a9cdd7011
Cyriacph <cyriac.philippe@epfl.ch>
2025-11-10 12:18:32 +0100

    chore: fix gradle defintions

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -148,7 +148,6 @@ dependencies {
     implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.ui)
     implementation(libs.androidx.ui.graphics)
-    implementation(libs.androidx.material3)
     implementation(libs.androidx.activity.compose)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.ui.tooling.preview)
