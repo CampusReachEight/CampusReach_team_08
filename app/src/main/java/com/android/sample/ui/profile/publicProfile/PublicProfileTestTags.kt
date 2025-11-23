00f4fd0ab27c9a14cb976792539718e0bbc73558
Cyriac <cyriacph@gmail.com>
2025-11-23 11:43:52 +0100

    feat(public-profile): add first test tags for public profile feature
    - Add test tags for follow button
    
    Note: additional test tags will be added as implementation continues

---
+++ app/src/main/java/com/android/sample/ui/profile/publicProfile/PublicProfileTestTags.kt
@@ -0,0 +1,8 @@
+package com.android.sample.ui.profile.publicProfile
+
+class PublicProfileTestTags {
+    companion object {
+        const val FOLLOW_BUTTON = "FollowButton"
+        const val UNFOLLOW_BUTTON = "UnfollowButton"
+    }
+}
