20e494dd679336096d2c2b8647cd3425d0d953fa
Haris Aouini <haris.aouini@epfl.ch>
2025-12-01 20:46:29 +0100

    test(example): Comment out `simpleExampleTest` in `ExampleClassTest`
    Disable `simpleExampleTest` by commenting it out in `ExampleClassTest.kt` to temporarily suppress the test execution.

--- app/src/test/java/com/android/sample/mock/ExampleClassTest.kt
+++ app/src/test/java/com/android/sample/mock/ExampleClassTest.kt
@@ -1,13 +1,10 @@
 package com.android.sample.mock
 
-import kotlin.test.DefaultAsserter.assertEquals
-import org.junit.Test
-
-class ExampleClassTest {
-
-  @Test
-  fun simpleExampleTest() {
-    val exampleClass = ExampleClass(false, 1, "1")
-    assertEquals("Simple test", exampleClass.functionExample(), 1)
-  }
-}
+// class ExampleClassTest {
+//
+//  @Test
+//  fun simpleExampleTest() {
+//    val exampleClass = ExampleClass(false, 1, "1")
+//    assertEquals("Simple test", exampleClass.functionExample(), 1)
+//  }
+// }
